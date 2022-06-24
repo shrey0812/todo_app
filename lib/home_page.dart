60e97514472a42a9298a995cbd378dd45c66aff6
Shrey Gupta <shreygupta0812@gmail>
2022-06-24 23:35:37 +0530

    intial commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -22,6 +22,68 @@ class homePage extends StatefulWidget {
 class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AppBar(
+                backgroundColor: const Color(0xfff96060),
+                elevation: 0,
+                title: const Text(
+                  'Work List',
+                  style: TextStyle(fontSize: 30),
+                ),
+                actions: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.short_text,
+                        color: Colors.white,
+                        size: 30,
+                      ))
+                ],
+              ),
+              Container(
+                height: 70,
+                color: const Color(0xfff96060),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                      InkWell(
+                        onTap: () {},
+                        child: const Text(
+                          'Today',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(height: 4, width: 120, color: Colors.white)
+                    ]),
+                    Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                      InkWell(
+                        onTap: () {},
+                        child: const Text(
+                          'Monthly',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(height: 4, width: 120, color: Colors.white)
+                    ])
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
